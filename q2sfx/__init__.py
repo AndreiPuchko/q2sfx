ff41bf17c8e0d98609b08330b6e6a665d7f9dad7
Andrei Puchko <andrei.puchko@gmx.de>
2025-12-27 00:11:15 +0100

    version.py

--- q2sfx/__init__.py
+++ q2sfx/__init__.py
@@ -1,10 +1,5 @@
 from importlib.metadata import version, PackageNotFoundError
-
-try:
-    __version__ = version("q2sfx")
-except PackageNotFoundError:
-    # package is not installed (e.g. editable / source checkout)
-    __version__ = "0.0.0"
+from version import __version__
 
 from .builder import Q2SFXBuilder
 
